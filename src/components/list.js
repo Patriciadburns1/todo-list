0181b4f6a4df697bba316218d0be6d3ae0650713
dburns1 <patriciadburns@gmail.com>
2018-06-26 15:00:57 -0700

    adding delete button functionality

--- src/components/list.js
+++ src/components/list.js
@@ -4,12 +4,13 @@ import Item from './item';
 
 //curly braces adding javascript - 
 export default props => {
+    // console.log("List Props", props); 
     const listElements = props.data.map((item, index) => {
         //index only thing available right now/ defeats purpose 
         // use somethign more unique like id - something must be unqiue
         // return <li className="collection-item" key={index}> {item.title} </li>
         // console.log('item', item); 
-        return <Item key={item._id} title={item.title} /> 
+        return <Item key={item._id} title={item.title} delete={()=> props.delete(item._id)} /> 
     })
     return(
        <ul className="collection"> 
