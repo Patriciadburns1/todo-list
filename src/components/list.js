0cc7bbd697bd47f019c85d41bcdbe0faf9198efd
dburns1 <patriciadburns@gmail.com>
2018-06-26 13:01:50 -0700

    delete button added and styling

--- src/components/list.js
+++ src/components/list.js
@@ -1,4 +1,5 @@
-import React from 'react'; 
+import React from 'react';
+import Item from './item';  
 
 
 //curly braces adding javascript - 
@@ -6,7 +7,9 @@ export default props => {
     const listElements = props.data.map((item, index) => {
         //index only thing available right now/ defeats purpose 
         // use somethign more unique like id - something must be unqiue
-        return <li className="collection-item" key={index}> {item.title} </li>
+        // return <li className="collection-item" key={index}> {item.title} </li>
+        // console.log('item', item); 
+        return <Item key={item._id} title={item.title} /> 
     })
     return(
        <ul className="collection"> 
